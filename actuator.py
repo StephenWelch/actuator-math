f57b3afea16db0d5d6195a66d5a5d1c6bf2aa359
Stephen Welch <stephenwelchva@vt.edu>
2022-02-26 17:10:07 -0500

    Working vis

--- actuator.py
+++ actuator.py
@@ -1,56 +1,137 @@
+from __future__ import annotations
 from dataclasses import dataclass
+import math
+import matplotlib
 import numpy as np
 from typing import List
+from scipy.spatial.transform import Rotation as R
+from mpl_toolkits.mplot3d.axes3d import Axes3D
+
+def rotate(origin, point, angle):
+    """
+    Rotate a point counterclockwise by a given angle around a given origin.
+
+    The angle should be given in radians.
+    """
+    ox, oy = origin
+    px, py = point
+
+    qx = ox + math.cos(angle) * (px - ox) - math.sin(angle) * (py - oy)
+    qy = oy + math.sin(angle) * (px - ox) + math.cos(angle) * (py - oy)
+    return qx, qy
 
 @dataclass
 class JointData:
-    joint_origin: np.ndarray
+    parent: JointData
+    origin: np.ndarray
     dof: np.ndarray
-    actuator_origins: List[np.ndarray]
+    actuator_origins: np.ndarray
 
-    actuator_forces: List[float] 
-    joint_torques: np.ndarray
+    actuator_forces: np.ndarray = np.array([])
+    torques: np.ndarray = np.array([])
+    angles: np.ndarray = np.array([])
 
     def __post_init__(self):
+        self.origin = np.array(self.origin)
+        self.dof = np.array(self.dof)
+        self.actuator_origins = np.array(self.actuator_origins)
+
         self.actuator_forces = [0.0] * len(self.actuator_origins)
-        self.joint_torques = np.zeros(self.dof.shape)
+        self.torques = np.zeros(self.dof.shape)
+        self.angles = np.zeros(self.dof.shape)
+
+    def num_dof(self) -> int:
+        return self.dof.count_nonzero()
+
+    def plot(self, ax: Axes3D) -> List:
+        origin = self.rotated_origin()
+        ax.scatter3D(origin[0], origin[1], origin[2])
+        actuator_origins = self.rotated_actuator_origins()
+        for i in range(len(actuator_origins)):
+            ax.plot3D(
+                actuator_origins[i, :, 0].flatten(),
+                actuator_origins[i, :, 1].flatten(), 
+                actuator_origins[i, :, 2].flatten())
+
+    def joint_rot_matrix(self) -> R:
+        return R.from_euler('XYZ', self.angles)
+        
+    def rotated_origin(self) -> np.ndarray:
+        # Rotate origin based on parent joint angle
+        if self.parent:
+            return self.parent.joint_rot_matrix().apply(self.origin - self.parent.rotated_origin()) + self.parent.rotated_origin()
+
+        return self.origin
+
+    def rotated_actuator_origins(self) -> np.ndarray:
+        actuator_origins = self.actuator_origins
+
+        actuator_origins = self.actuator_origins - self.origin
+        for r in range(actuator_origins.shape[0]):
+            actuator_origins[r][1] = self.joint_rot_matrix().apply(actuator_origins[r][1])
+        actuator_origins = actuator_origins + self.origin
+        
+        if self.parent:
+            actuator_origins = actuator_origins - self.parent.rotated_origin()
+            for r in range(actuator_origins.shape[0]):
+                actuator_origins[r, :] = self.parent.joint_rot_matrix().apply(actuator_origins[r, :])
+            actuator_origins = actuator_origins + self.parent.rotated_origin()
+
+
+        
+
+        return actuator_origins
+
+
 
 RL1_HIP_YAW_ROL = JointData(
-    np.array([-119, 0, -275]),
-    np.array([0, 0, 1]),
+    None,
+    [-119, 0, -275],
+    [0, 0, 1],
     [
-        np.array([-47, 60, -54], [-162, 59, -263]),
-        np.array([-47, -60, -54], [-160, -62, -263])
+        [[-47, 60, -54], [-162, 59, -263]],
+        [[-47, -60, -54], [-160, -62, -263]]
     ]
 )
 
 RL3_HIP_PIT = JointData(
-    np.array([-119, 1.7, -330])
-    np.array([0, 1, 0]),
+    RL1_HIP_YAW_ROL,
+    [-119, 1.7, -330],
+    [0, 1, 0],
     [
-        np.array([-87, -44, -343], [-87, 87, -638])
+        [[-87, -44, -343], [-87, 87, -638]]
     ]
 )
 
 RL4_KNE_PIT = JointData(
-    np.array([-119, 2.5, -778]), 
-    np.array([0, 1, 0]),
+    RL3_HIP_PIT,
+    [-119, 2.5, -778], 
+    [0, 1, 0],
     [
-        np.array([-119, -44, -408], [-119, -57, -834])
+        [[-119, -44, -408], [-119, -57, -834]]
     ]
 )
 
 RL5_ANK = JointData(
-    np.array([-119, 6, -1213]),
-    np.array([1, 1, 0]),
+    RL4_KNE_PIT,
+    [-119, 6, -1213],
+    [1, 1, 0],
     [  
-        np.array([-63, 48, -866], [-65, -67, -1188]),
-        np.array([-175, 48, -866], [-175, -67, -1188])
+        [ [-63, 48, -866], [-65, -67, -1188] ],
+        [ [-175, 48, -866], [-175, -67, -1188] ]
     ]
 )
 
+ALL_JOINTS = [RL1_HIP_YAW_ROL, RL3_HIP_PIT, RL4_KNE_PIT, RL5_ANK]
+
 def force_to_torque(joint: JointData):
-    ...
+    actuator_vecs = joint.actuator_forces[:, 1] - joint.actuator_forces[:, 0]
+    actuator_forces = np.linalg.norm(actuator_vecs, axis=2).dot(joint.actuator_forces)
+    return np.cross(joint.rotated_actuator_origins(), actuator_forces)
+
+
 
 def torque_to_force(joint: JointData):
-    ...
+    actuator_vecs = joint.actuator_forces[:, 1] - joint.actuator_forces[:, 0]
+    actuator_forces = np.linalg.norm(joint.rotated_actuator_origins(), axis=2).dot(joint.actuator_forces)
+    return np.cross(actuator_forces, joint.torques) / actuator_vecs ** 2
--- main.py
+++ main.py
@@ -1,7 +1,44 @@
+import math
 import numpy as np
 import matplotlib.pyplot as plt
+from actuator import ALL_JOINTS, RL4_KNE_PIT, JointData, force_to_torque, torque_to_force
+from mpl_toolkits import mplot3d
+from matplotlib.widgets import Slider, Button
+
+
 
 def main():
+    ax = plt.axes(projection='3d')
+    plt.subplots_adjust(bottom=0.25)
+
+    knee_pitch = Slider(
+        ax=plt.axes([0.25, 0.1, 0.65, 0.03]),
+        label='Knee Angle',
+        valmin=-90,
+        valmax=90,
+        valinit=0,
+    )
+
+    def plot_all():
+        ax.axes.set_xlim3d(left=-500, right=500) 
+        ax.axes.set_ylim3d(bottom=-500, top=500) 
+        ax.axes.set_zlim3d(bottom=-1500, top=0) 
+        for joint in ALL_JOINTS:
+            joint.plot(ax)
+
+        limits = np.array([getattr(ax, f'get_{axis}lim')() for axis in 'xyz'])
+        ax.set_box_aspect(np.ptp(limits, axis = 1))
+
+    def update_knee_angle(val):
+        RL4_KNE_PIT.angles[0] = math.radians(knee_pitch.val)
+        ax.clear()
+        plot_all()
+
+    knee_pitch.on_changed(update_knee_angle)
+
+    plot_all()
+
+    plt.show()
 
 if __name__ == '__main__':
     main()
